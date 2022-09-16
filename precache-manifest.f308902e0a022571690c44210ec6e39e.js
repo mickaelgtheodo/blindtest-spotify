92f3ea2843f88fce04a5d5692ac315bce465d124
Mickaël Gregot <mickaelg@theodo.fr>
2022-09-16 16:35:35 +0200

    Updates

---
+++ precache-manifest.f308902e0a022571690c44210ec6e39e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea874cc864e8fe4c1e26c12f9761fc8f",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "2a8756eab8e1d0c85ae2",
+    "url": "/blindtest-spotify/static/css/main.348ad1c0.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8756eab8e1d0c85ae2",
+    "url": "/blindtest-spotify/static/js/main.3e954202.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
